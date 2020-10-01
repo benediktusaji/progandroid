fcca563b0e0a41ed4d63751549e7532392e9030e
benediktusaji <44857388+benediktusaji@users.noreply.github.com>
2020-10-01 21:15:56 +0700

    -Add simple changes

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+//Ini contoh komentar
+    private static final String TAG = "Aktivitasku";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Aktivitasku","Check");
         setContentView(R.layout.activity_main);
     }
 }
