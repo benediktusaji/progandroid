fd37e1beda9338869ce91d8eedadc558d7da38d0
benediktusaji <44857388+benediktusaji@users.noreply.github.com>
2020-10-15 21:22:38 +0700

    Home Screen
    - Add 2 Fragments to Home Screen

--- app/src/main/java/com/example/helloworld/LoginActivity.java
+++ app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -4,20 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 //Ini contoh komentar
     private static final String TAG = "Aktivitasku";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        Log.d("Aktivitasku","Check");
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         final Button button = findViewById(R.id.btnLogin);
         final EditText txtUsername = findViewById(R.id.editTextUsername);
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 // Code here executes on main thread after user presses button
                 if(txtUsername.getText().toString().equals("admin")&&txtPassword.getText().toString().equals("admin")){
                     // Masuk ke activity baru
-                    Intent intent = new Intent(v.getContext(), HomeScreen.class);
+                    Intent intent = new Intent(v.getContext(), HomeActivity.class);
                     startActivity(intent);
                 }else{
                     Toast.makeText(getApplicationContext(),"Username atau Password Anda tidak benar!",Toast.LENGTH_LONG).show();
