bc190d05be3d373de6d550d72d296b5dc4546144
benediktusaji <44857388+benediktusaji@users.noreply.github.com>
2020-11-05 21:22:34 +0700

    RecyclerView
    Create RecycleView inside right fragment about movie items.

--- app/src/main/java/com/example/helloworld/FragmentRight.java
+++ app/src/main/java/com/example/helloworld/FragmentRight.java
@@ -8,11 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class FragmentRight extends Fragment {
+
+    RecyclerView recyclerView;
+    String s1[], s2[];
+    int images[] = {R.drawable.brba,R.drawable.bcs,R.drawable.narcos};
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = getView().findViewById(R.id.recyclerView);
+        MyAdapter myAdapter = new MyAdapter(getActivity(), s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        s1 = getResources().getStringArray(R.array.movie_name);
+        s2 = getResources().getStringArray(R.array.movie_desc);
         return inflater.inflate(R.layout.fragment_right, container, false);
     }
 }
---
+++ app/src/main/java/com/example/helloworld/MyAdapter.java
@@ -0,0 +1,58 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String s1[], s2[];
+    int img[];
+    Context context;
+
+    public MyAdapter(Context context, String s1[], String s2[], int img[]){
+        this.context = context;
+        this.s1 = s1;
+        this.s2 = s2;
+        this.img = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.movieTitle.setText(s1[position]);
+        holder.movieDesc.setText(s2[position]);
+        holder.moviePoster.setImageResource(img[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return s1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView movieTitle, movieDesc;
+        ImageView moviePoster;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieTitle = itemView.findViewById(R.id.movieTitle);
+            movieDesc = itemView.findViewById(R.id.movieDesc);
+            moviePoster = itemView.findViewById(R.id.moviePoster);
+        }
+    }
+}
